1fe185cf8cafd1419b444499af3230a5f6d8eb34
Jenaide <jenaidesibolie@gmail.com>
2023-08-10 18:49:09 +0000

    bug fix

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,5 +1,5 @@
---a script that creates a view need_meeting that lists all students that have a score
---under 80 (strict) and no last_meeting or more than 1 month.
+-- a script that creates a view need_meeting that lists all students that have a score
+-- under 80 (strict) and no last_meeting or more than 1 month.
 DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
     SELECT name
