6441bd32136c78490c12de2367c06e6b3d535d37
Jenaide <jenaidesibolie@gmail.com>
2023-08-17 14:24:15 +0000

    added additional task

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Created by Jenaide Sibolie
+"""
+import redis
+import requests
+from typing import Callable
+from functools import wraps
+
+
+redis_inst = redis.Redis()
+
+
+def cacher(method: Callable) -> Callable:
+    """
+    caches the output data when fetched
+    """
+    @wrap(method)
+    def invoke(url) -> str:
+        """
+        a wrapper function for caching the output
+        """
+        redis_inst.incr(f'count:{url}')
+        results = redis_inst.get(f'results:{url}')
+        if results:
+            return results.decode('utf-8')
+        results = method(url)
+        redis_inst.set(f'count:{url}', 0)
+        redis_inst.setex(f'results:{urls}', 10, results)
+        return results
+    return invoke
+
+@cacher
+def get_page(url: str) -> str:
+    """
+    function that returns the content of a url after caching the request
+    """
+    return requests.get(url).text
